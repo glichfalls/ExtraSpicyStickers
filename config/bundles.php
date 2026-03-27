011002031f23e82eb7a8790b4ecb80d78ea0f6cc
robin <robin.portner@protonmail.com>
2026-03-27 21:41:06 +0100

    fix: register SentryBundle in bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     BoShurik\TelegramBotBundle\BoShurikTelegramBotBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
